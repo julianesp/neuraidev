c0ed4141e73051eeada1d49071db5e4a0ce16725
Julián España <julii1295@gmail.com>
2024-10-07 22:05:01 -0500

    Agregando imágenes a página de servicios

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,6 +3,7 @@
 import { React, useState, useEffect } from "react";
 import RootLayout from "@/app/layout";
 import Link from "next/link";
+import ImageSlider from "@/containers/ImageSlider";
 import styles from "@/styles/Services.module.scss";
 
 const Services = () => {
@@ -13,10 +14,25 @@ const Services = () => {
   }, []);
   if (!isLoaded) return null;
 
+  // images to show
+  const services = [
+    {
+      d1: "/images/formateo.png",
+    },
+    {
+      d2: "/images/ssd.png",
+    },
+    {
+      d3: "/images/ssd.png",
+    },
+  ];
+
+  const imagePaths = [services[0].d1, services[1].d2, services[2].d3];
+
   return (
     <RootLayout>
       <section className={styles.services}>
-        <p>Crear carrusel para presentar los siguientes servicios</p>
+        <ImageSlider imagePaths={imagePaths} enableTransition={false} />
         <ul>
           <li>Formateo PC</li>
           <li>Mantenimiento PC</li>
