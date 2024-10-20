28436a6bd3a03ff412948d17f15603d28200e807
Julián España <julii1295@gmail.com>
2024-10-19 20:42:06 -0500

    Agregando espacio para mostrar los sitios web que he creado

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,6 +5,7 @@ import RootLayout from "@/app/layout";
 import Link from "next/link";
 import ImageSlider from "@/containers/ImageSlider";
 import styles from "@/styles/Services.module.scss";
+import Image from "next/image";
 
 const Services = () => {
   // Solution for hydratation errors
@@ -26,8 +27,18 @@ const Services = () => {
       d3: "/images/ssd.png",
     },
   ];
-
   const imagePaths = [services[0].d1, services[1].d2, services[2].d3];
+
+  const imagesSites = [
+    {
+      contab: "/images/budget.png",
+    },
+    {
+      ase: "/images/logo_ase.jpg",
+    },
+  ];
+
+  const sitesWeb = [imagesSites[0].contab, imagesSites[1].ase];
 
   return (
     <RootLayout>
@@ -45,9 +56,31 @@ const Services = () => {
           <li>Este texto indica las imagenes que van en el slider</li>
         </ul>
 
-        <p>Space for presentation my websites</p>
         <p>Space for presentation formateo service</p>
         <p>Space for presentation Mantenimiento service</p>
+      </section>
+
+      <section className={styles.projects}>
+        <h2>Mis proyectos</h2>
+
+        <article>
+          <div>
+            <Link href="https://julianesp.github.io/contavsib/" target="_blank">
+              <Image
+                alt="Logo contavsib"
+                src={sitesWeb[0]}
+                width={50}
+                height={50}
+              />
+            </Link>
+          </div>
+
+          <div>
+            <Link href="https://julianesp.github.io/ase/" target="_blank">
+              <Image alt="Logo ase" src={sitesWeb[1]} width={50} height={50} />
+            </Link>
+          </div>
+        </article>
       </section>
     </RootLayout>
   );
