4a07f6d96036b318d75cc7b685169159d51a5be6
Julián España <julii1295@gmail.com>
2025-02-11 20:17:51 -0500

    Agregando archivos e información faltante

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,24 @@ export default function RootLayout({ children }) {
   return (
     <React.Fragment>
       <html>
+        <head>
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <meta name="apple-mobile-web-app-title" content="neurai.dev" />
+          <link rel="manifest" href="/manifest.json" />
+        </head>
+
         <body>
           {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
