d1da893c7d832e98da686f2756da4c6c5dd3370a
Julián España <julii1295@gmail.com>
2025-02-11 20:05:05 -0500

    Agregando favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,19 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 export default function RootLayout({ children }) {
+  manifest: "/manifest.json";
+  themeColor: "#ffffff";
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ];
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ];
+  }
+
   return (
     <React.Fragment>
       <html>
