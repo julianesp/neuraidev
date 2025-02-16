b1cfbd6e557e76caac6a213e08b682efa03e1351
Julián España <julii1295@gmail.com>
2025-02-15 21:57:25 -0500

    Subiendo imágenes de presentación

--- src/components/CarouselDemo.js
+++ src/components/CarouselDemo.js
@@ -15,44 +15,26 @@ import styles from "@/styles/Carousel.module.scss";
 
 const images = [
   "https://nwxetoffoghsimkqfsbv.supabase.co/storage/v1/object/sign/media/presentation/presentationFormateo.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYS9wcmVzZW50YXRpb24vcHJlc2VudGF0aW9uRm9ybWF0ZW8uanBnIiwiaWF0IjoxNzM5NTg4ODIxLCJleHAiOjE3NzExMjQ4MjF9.Q3S1LMTRV3mQrxdl-jktDMClc6corCx3OyK6H1Fh7E8",
-      
-  "https://nwxetoffoghsimkqfsbv.supabase.co/storage/v1/object/sign/media/presentation/studio.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYS9wcmVzZW50YXRpb24vc3R1ZGlvLnBuZyIsImlhdCI6MTczOTU4ODg1NSwiZXhwIjoxNzcxMTI0ODU1fQ.o18MzlbjUguamnHOkdWpCx-EvHmhRadXegr-DxnHSK4g",
-  "https://nwxetoffoghsimkqfsbv.supabase.co/storage/v1/object/sign/media/presentation/books.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYS9wcmVzZW50YXRpb24vYm9va3MuanBnIiwiaWF0IjoxNzM5NTg4ODY5LCJleHAiOjE3NzExMjQ4Njl9.gr7lGYZFmUykkGQVWZeTpBOCSJz-DVlpggBgW65WAiY",
-  "/images/image4.jpg",
-  "/images/image5.jpg",
+
+  "https://nwxetoffoghsimkqfsbv.supabase.co/storage/v1/object/sign/media/presentation/contavsib.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYS9wcmVzZW50YXRpb24vY29udGF2c2liLmpwZyIsImlhdCI6MTczOTY3NDQ5OSwiZXhwIjoxNzcxMjEwNDk5fQ.z8xKrVLotc3If3bLViN_S1cR28d6HJlFCIja180pThY",
+  "https://nwxetoffoghsimkqfsbv.supabase.co/storage/v1/object/sign/media/presentation/desarrollo.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJtZWRpYS9wcmVzZW50YXRpb24vZGVzYXJyb2xsby5qcGciLCJpYXQiOjE3Mzk2NzQ1NjUsImV4cCI6MTc3MTIxMDU2NX0.GmirLAxZCmiNmHDV5ZqujSmUufMH95DtTpBVEtWY0GM",
 ];
 
 export function CarouselDemo() {
   return (
-    // <Carousel className={`${styles.carusel} w-full `}>
-    //   <CarouselContent>
-    //     {Array.from({ length: 5 }).map((_, index) => (
-    //       <CarouselItem key={index}>
-    //         <div className="p-1">
-    //           <Card>
-    //             <CardContent className="flex aspect-square items-center justify-center p-6">
-    //               <span className="text-4xl font-semibold">{index + 1}</span>
-    //             </CardContent>
-    //           </Card>
-    //         </div>
-    //       </CarouselItem>
-    //     ))}
-    //   </CarouselContent>
-    //   <CarouselPrevious />
-    //   <CarouselNext />
-    // </Carousel>
-
     <Carousel className={`${styles.carusel} w-full`}>
       <CarouselContent>
         {images.map((src, index) => (
           <CarouselItem key={index}>
             <div className="p-1">
-              <Card className="w-full h-full">
-                <CardContent className="flex items-center justify-center p-6 w-full h-full">
+              <Card className="">
+                <CardContent
+                  className={`${styles.image} flex items-center justify-center p-0 w-full  h-48 sm:h-60 md:h-80`}
+                >
                   <Image
                     src={src}
                     alt={`Imagen ${index + 1}`}
-                    className="w-full h-full object-cover rounded-lg"
+                    className="w-full h-full object-cover rounded-lg "
                     width={320}
                     height={320}
                   />
