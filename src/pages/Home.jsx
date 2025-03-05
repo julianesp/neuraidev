d9534cb41af86d1bee36f963a07d95e89898038a
Julián España <julii1295@gmail.com>
2025-03-04 21:01:48 -0500

    Arreglando ancho de accesorios destacados en vista mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -150,7 +150,7 @@ const Inicio = () => {
           <CarouselDemo apiUrl={API_CELULARES} />
         </section>
 
-        <section>
+        <section className={styles.destacados}>
           <AccesoriosDestacados
             accesorios={accesoriosData}
             mostrarDescripcion={false}
