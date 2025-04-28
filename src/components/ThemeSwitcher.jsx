b8125bee8f2385c7400c9f29812717f6a8a1d430
julii1295@gmail.com <julii1295@gmail.com>
2025-04-27 21:20:53 -0500

    Corrigiendo error de hidratacion

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -3,9 +3,17 @@
 import { useTheme } from "next-themes";
 import { Sun, Moon } from "lucide-react"; // Íconos bonitos
 import { Button } from "@/components/ui/button"; // Botón estilizado de shadcn
+import { useEffect, useState } from "react";
 
 export default function ThemeSwitcher() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null; // O un loader, o solo el botón sin icono
 
   return (
     <Button
