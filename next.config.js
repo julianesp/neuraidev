4b4b88314ee4c45b6f9cd030745b0103c7e3d9fc
julianesp <julii1295@gmail.com>
2026-03-26 11:38:59 -0500

    Arreglando carga de imágenes

--- next.config.js
+++ next.config.js
@@ -152,7 +152,7 @@ const nextConfig = {
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    unoptimized: false,
+    unoptimized: true,
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/webp"],
